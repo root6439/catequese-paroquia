6b73c74be38afd65cb679754cfeeb494dafc91ae
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2022-04-10 14:34:18 -0300

    Refatorada home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
@@ -5,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { ROUTES } from './home.routes';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -13,6 +15,8 @@ import { ROUTES } from './home.routes';
     RouterModule.forChild(ROUTES),
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
   ],
 })
 export class HomeModule {}
